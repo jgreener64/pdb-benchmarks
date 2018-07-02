eef1b51e7b4627b1015b7fcb6948cc9b923b131a
Joe Greener <jgreener@hotmail.co.uk>
2018-07-02 16:43:12 +0100

    change C++ timing method

--- ESBTL/parse.cc
+++ ESBTL/parse.cc
@@ -7,12 +7,13 @@
 int main( int argc, char* argv[] ) {
     std::string pdb_filepath = argv[1];
     std::cout.setstate(std::ios_base::failbit);
-    clock_t tStart = clock();
+    struct timespec tstart, tend;
+    clock_gettime(CLOCK_REALTIME, &tstart);
     ESBTL::PDB_line_selector sel;
     std::vector<ESBTL::Default_system> systems;
     ESBTL::All_atom_system_builder<ESBTL::Default_system> builder(systems, sel.max_nb_systems());
     ESBTL::read_a_pdb_file(pdb_filepath, sel, builder, ESBTL::Accept_none_occupancy_policy<ESBTL::PDB::Line_format<> >());
-    clock_t tFinish = clock();
+    clock_gettime(CLOCK_REALTIME, &tend);
     std::cout.clear();
-    printf("%.6f\n", (double)(tFinish - tStart)/CLOCKS_PER_SEC);
+    printf("%.6f\n", (tend.tv_sec - tstart.tv_sec) + (tend.tv_nsec - tstart.tv_nsec)/1E9);
 }
--- Victor/parse.cc
+++ Victor/parse.cc
@@ -10,13 +10,14 @@ using namespace Victor;
 
 int main( int argc, char* argv[] ) {
     string pdb_filepath = argv[1];
-    clock_t tStart = clock();
+    struct timespec tstart, tend;
+    clock_gettime(CLOCK_REALTIME, &tstart);
     ifstream inFile( pdb_filepath.c_str() );
     // See options at
     // http://protein.bio.unipd.it/victor_doxygen/classVictor_1_1Biopool_1_1PdbLoader.html
     PdbLoader pl(inFile, true, true, false, true, false, false, false, true);
     Protein prot;
     prot.load( pl );
-    clock_t tFinish = clock();
-    printf("%.6f\n", (double)(tFinish - tStart)/CLOCKS_PER_SEC);
+    clock_gettime(CLOCK_REALTIME, &tend);
+    printf("%.6f\n", (tend.tv_sec - tstart.tv_sec) + (tend.tv_nsec - tstart.tv_nsec)/1E9);
 }
