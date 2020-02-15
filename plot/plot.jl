2d6c4470e7786b3e8723c5afa5f0a2d347e2163e
Joe Greener <jgreener@hotmail.co.uk>
2020-02-15 20:59:33 +0000

    nicer plot, capital letters

--- plot/plot.jl
+++ plot/plot.jl
@@ -5,17 +5,26 @@ using Gadfly
 using Cairo
 using Fontconfig
 
-df = CSV.read("benchmarks.csv")
+df = CSV.read("C:\\Users\\Joe\\software\\pdb-benchmarks\\benchmarks.csv")
 
-p = plot(df,
-    x=:package,
-    y=:time,
-    color=:benchmark,
-    Scale.y_log10,
-    Guide.xlabel(nothing),
-    Guide.ylabel("time / s"),
-    Geom.point,
-    Geom.line,
+theme = Theme(
+    background_color="white",
+    panel_stroke="black",
+    major_label_color="black",
+    minor_label_color="black",
 )
 
-draw(PNG("plot/plot.png", 9inch, 6inch), p)
+p = Gadfly.with_theme(theme) do
+    plot(df,
+        x=:Package,
+        y=:Runtime,
+        color=:Benchmark,
+        Scale.y_log10,
+        Guide.xlabel(nothing),
+        Guide.ylabel("Runtime / s"),
+        Geom.point,
+        Geom.line,
+    )
+end
+
+draw(PNG("C:\\Users\\Joe\\software\\pdb-benchmarks\\plot\\plot.png", dpi=300), p)
--- tools/run_benchmarks.sh
+++ tools/run_benchmarks.sh
@@ -15,7 +15,7 @@ rm */*.dat
 rm plot/plot.png
 
 # Reset benchmarking results
-echo "package,benchmark,time" > benchmarks.csv
+echo "Package,Benchmark,Runtime" > benchmarks.csv
 
 # Run a benchmark
 # Arguments are number of runs, benchmark command, output data file, csv file columns
@@ -29,90 +29,90 @@ function run_benchmark {
 }
 
 # BioStructures
-run_benchmark $nb "julia BioStructures/parse.jl pdbs/1CRN.pdb"    "BioStructures/parse_1CRN.dat"     "BioStructures,parse 1CRN,"
-run_benchmark $nb "julia BioStructures/parse.jl pdbs/3JYV.pdb"    "BioStructures/parse_3JYV.dat"     "BioStructures,parse 3JYV,"
-run_benchmark $ns "julia BioStructures/parse.jl pdbs/1HTQ.pdb"    "BioStructures/parse_1HTQ.dat"     "BioStructures,parse 1HTQ,"
-run_benchmark $nb "julia BioStructures/count.jl"                  "BioStructures/count.dat"          "BioStructures,count,"
-run_benchmark $nb "julia BioStructures/distance.jl"               "BioStructures/distance.dat"       "BioStructures,distance,"
-run_benchmark $nb "julia BioStructures/ramachandran.jl"           "BioStructures/ramachandran.dat"   "BioStructures,ramachandran,"
+run_benchmark $nb "julia BioStructures/parse.jl pdbs/1CRN.pdb"    "BioStructures/parse_1CRN.dat"     "BioStructures,Parse 1CRN,"
+run_benchmark $nb "julia BioStructures/parse.jl pdbs/3JYV.pdb"    "BioStructures/parse_3JYV.dat"     "BioStructures,Parse 3JYV,"
+run_benchmark $ns "julia BioStructures/parse.jl pdbs/1HTQ.pdb"    "BioStructures/parse_1HTQ.dat"     "BioStructures,Parse 1HTQ,"
+run_benchmark $nb "julia BioStructures/count.jl"                  "BioStructures/count.dat"          "BioStructures,Count,"
+run_benchmark $nb "julia BioStructures/distance.jl"               "BioStructures/distance.dat"       "BioStructures,Distance,"
+run_benchmark $nb "julia BioStructures/ramachandran.jl"           "BioStructures/ramachandran.dat"   "BioStructures,Ramachandran,"
 echo "BioStructures benchmarks done"
 
 # MIToS
-run_benchmark $nb "julia MIToS/parse.jl pdbs/1CRN.pdb"      "MIToS/parse_1CRN.dat"        "MIToS,parse 1CRN,"
-run_benchmark $nb "julia MIToS/parse.jl pdbs/3JYV.pdb"      "MIToS/parse_3JYV.dat"        "MIToS,parse 3JYV,"
-run_benchmark $ns "julia MIToS/parse.jl pdbs/1HTQ.pdb"      "MIToS/parse_1HTQ.dat"        "MIToS,parse 1HTQ,"
-run_benchmark $nb "julia MIToS/count.jl"                    "MIToS/count.dat"             "MIToS,count,"
-run_benchmark $nb "julia MIToS/distance.jl"                 "MIToS/distance.dat"          "MIToS,distance,"
+run_benchmark $nb "julia MIToS/parse.jl pdbs/1CRN.pdb"      "MIToS/parse_1CRN.dat"        "MIToS,Parse 1CRN,"
+run_benchmark $nb "julia MIToS/parse.jl pdbs/3JYV.pdb"      "MIToS/parse_3JYV.dat"        "MIToS,Parse 3JYV,"
+run_benchmark $ns "julia MIToS/parse.jl pdbs/1HTQ.pdb"      "MIToS/parse_1HTQ.dat"        "MIToS,Parse 1HTQ,"
+run_benchmark $nb "julia MIToS/count.jl"                    "MIToS/count.dat"             "MIToS,Count,"
+run_benchmark $nb "julia MIToS/distance.jl"                 "MIToS/distance.dat"          "MIToS,Distance,"
 echo "MIToS benchmarks done"
 
 # Biopython
-run_benchmark $nb "python Biopython/parse.py pdbs/1CRN.pdb"  "Biopython/parse_1CRN.dat"    "Biopython,parse 1CRN,"
-run_benchmark $nb "python Biopython/parse.py pdbs/3JYV.pdb"  "Biopython/parse_3JYV.dat"    "Biopython,parse 3JYV,"
-run_benchmark $ns "python Biopython/parse.py pdbs/1HTQ.pdb"  "Biopython/parse_1HTQ.dat"    "Biopython,parse 1HTQ,"
-run_benchmark $nb "python Biopython/count.py"                "Biopython/count.dat"         "Biopython,count,"
-run_benchmark $nb "python Biopython/distance.py"             "Biopython/distance.dat"      "Biopython,distance,"
-run_benchmark $nb "python Biopython/ramachandran.py"         "Biopython/ramachandran.dat"  "Biopython,ramachandran,"
+run_benchmark $nb "python Biopython/parse.py pdbs/1CRN.pdb"  "Biopython/parse_1CRN.dat"    "Biopython,Parse 1CRN,"
+run_benchmark $nb "python Biopython/parse.py pdbs/3JYV.pdb"  "Biopython/parse_3JYV.dat"    "Biopython,Parse 3JYV,"
+run_benchmark $ns "python Biopython/parse.py pdbs/1HTQ.pdb"  "Biopython/parse_1HTQ.dat"    "Biopython,Parse 1HTQ,"
+run_benchmark $nb "python Biopython/count.py"                "Biopython/count.dat"         "Biopython,Count,"
+run_benchmark $nb "python Biopython/distance.py"             "Biopython/distance.dat"      "Biopython,Distance,"
+run_benchmark $nb "python Biopython/ramachandran.py"         "Biopython/ramachandran.dat"  "Biopython,Ramachandran,"
 echo "Biopython benchmarks done"
 
 # ProDy
-run_benchmark $nb "python ProDy/parse.py pdbs/1CRN.pdb"      "ProDy/parse_1CRN.dat"        "ProDy,parse 1CRN,"
-run_benchmark $nb "python ProDy/parse.py pdbs/3JYV.pdb"      "ProDy/parse_3JYV.dat"        "ProDy,parse 3JYV,"
-run_benchmark $ns "python ProDy/parse.py pdbs/1HTQ.pdb"      "ProDy/parse_1HTQ.dat"        "ProDy,parse 1HTQ,"
-run_benchmark $nb "python ProDy/count.py"                    "ProDy/count.dat"             "ProDy,count,"
-run_benchmark $nb "python ProDy/distance.py"                 "ProDy/distance.dat"          "ProDy,distance,"
-run_benchmark $nb "python ProDy/ramachandran.py"             "ProDy/ramachandran.dat"      "ProDy,ramachandran,"
+run_benchmark $nb "python ProDy/parse.py pdbs/1CRN.pdb"      "ProDy/parse_1CRN.dat"        "ProDy,Parse 1CRN,"
+run_benchmark $nb "python ProDy/parse.py pdbs/3JYV.pdb"      "ProDy/parse_3JYV.dat"        "ProDy,Parse 3JYV,"
+run_benchmark $ns "python ProDy/parse.py pdbs/1HTQ.pdb"      "ProDy/parse_1HTQ.dat"        "ProDy,Parse 1HTQ,"
+run_benchmark $nb "python ProDy/count.py"                    "ProDy/count.dat"             "ProDy,Count,"
+run_benchmark $nb "python ProDy/distance.py"                 "ProDy/distance.dat"          "ProDy,Distance,"
+run_benchmark $nb "python ProDy/ramachandran.py"             "ProDy/ramachandran.dat"      "ProDy,Ramachandran,"
 echo "ProDy benchmarks done"
 
 # MDAnalysis
-run_benchmark $nb "python MDAnalysis/parse.py pdbs/1CRN.pdb" "MDAnalysis/parse_1CRN.dat"   "MDAnalysis,parse 1CRN,"
-run_benchmark $nb "python MDAnalysis/parse.py pdbs/3JYV.pdb" "MDAnalysis/parse_3JYV.dat"   "MDAnalysis,parse 3JYV,"
-run_benchmark $ns "python MDAnalysis/parse.py pdbs/1HTQ.pdb" "MDAnalysis/parse_1HTQ.dat"   "MDAnalysis,parse 1HTQ,"
-run_benchmark $nb "python MDAnalysis/count.py"               "MDAnalysis/count.dat"        "MDAnalysis,count,"
-run_benchmark $nb "python MDAnalysis/distance.py"            "MDAnalysis/distance.dat"     "MDAnalysis,distance,"
-run_benchmark $nb "python MDAnalysis/ramachandran.py"        "MDAnalysis/ramachandran.dat" "MDAnalysis,ramachandran,"
+run_benchmark $nb "python MDAnalysis/parse.py pdbs/1CRN.pdb" "MDAnalysis/parse_1CRN.dat"   "MDAnalysis,Parse 1CRN,"
+run_benchmark $nb "python MDAnalysis/parse.py pdbs/3JYV.pdb" "MDAnalysis/parse_3JYV.dat"   "MDAnalysis,Parse 3JYV,"
+run_benchmark $ns "python MDAnalysis/parse.py pdbs/1HTQ.pdb" "MDAnalysis/parse_1HTQ.dat"   "MDAnalysis,Parse 1HTQ,"
+run_benchmark $nb "python MDAnalysis/count.py"               "MDAnalysis/count.dat"        "MDAnalysis,Count,"
+run_benchmark $nb "python MDAnalysis/distance.py"            "MDAnalysis/distance.dat"     "MDAnalysis,Distance,"
+run_benchmark $nb "python MDAnalysis/ramachandran.py"        "MDAnalysis/ramachandran.dat" "MDAnalysis,Ramachandran,"
 echo "MDAnalysis benchmarks done"
 
 # Bio3D
-run_benchmark $nb "Rscript Bio3D/parse.R pdbs/1CRN.pdb"      "Bio3D/parse_1CRN.dat"        "Bio3D,parse 1CRN,"
-run_benchmark $nb "Rscript Bio3D/parse.R pdbs/3JYV.pdb"      "Bio3D/parse_3JYV.dat"        "Bio3D,parse 3JYV,"
-run_benchmark $ns "Rscript Bio3D/parse.R pdbs/1HTQ.pdb"      "Bio3D/parse_1HTQ.dat"        "Bio3D,parse 1HTQ,"
-run_benchmark $nb "Rscript Bio3D/count.R"                    "Bio3D/count.dat"             "Bio3D,count,"
-run_benchmark $nb "Rscript Bio3D/distance.R"                 "Bio3D/distance.dat"          "Bio3D,distance,"
+run_benchmark $nb "Rscript Bio3D/parse.R pdbs/1CRN.pdb"      "Bio3D/parse_1CRN.dat"        "Bio3D,Parse 1CRN,"
+run_benchmark $nb "Rscript Bio3D/parse.R pdbs/3JYV.pdb"      "Bio3D/parse_3JYV.dat"        "Bio3D,Parse 3JYV,"
+run_benchmark $ns "Rscript Bio3D/parse.R pdbs/1HTQ.pdb"      "Bio3D/parse_1HTQ.dat"        "Bio3D,Parse 1HTQ,"
+run_benchmark $nb "Rscript Bio3D/count.R"                    "Bio3D/count.dat"             "Bio3D,Count,"
+run_benchmark $nb "Rscript Bio3D/distance.R"                 "Bio3D/distance.dat"          "Bio3D,Distance,"
 echo "Bio3D benchmarks done"
 
 # Rpdb
-run_benchmark $nb "Rscript Rpdb/parse.R pdbs/1CRN.pdb"       "Rpdb/parse_1CRN.dat"         "Rpdb,parse 1CRN,"
-run_benchmark $nb "Rscript Rpdb/parse.R pdbs/3JYV.pdb"       "Rpdb/parse_3JYV.dat"         "Rpdb,parse 3JYV,"
-run_benchmark $ns "Rscript Rpdb/parse.R pdbs/1HTQ.pdb"       "Rpdb/parse_1HTQ.dat"         "Rpdb,parse 1HTQ,"
-run_benchmark $nb "Rscript Rpdb/count.R"                     "Rpdb/count.dat"              "Rpdb,count,"
-run_benchmark $nb "Rscript Rpdb/distance.R"                  "Rpdb/distance.dat"           "Rpdb,distance,"
+run_benchmark $nb "Rscript Rpdb/parse.R pdbs/1CRN.pdb"       "Rpdb/parse_1CRN.dat"         "Rpdb,Parse 1CRN,"
+run_benchmark $nb "Rscript Rpdb/parse.R pdbs/3JYV.pdb"       "Rpdb/parse_3JYV.dat"         "Rpdb,Parse 3JYV,"
+run_benchmark $ns "Rscript Rpdb/parse.R pdbs/1HTQ.pdb"       "Rpdb/parse_1HTQ.dat"         "Rpdb,Parse 1HTQ,"
+run_benchmark $nb "Rscript Rpdb/count.R"                     "Rpdb/count.dat"              "Rpdb,Count,"
+run_benchmark $nb "Rscript Rpdb/distance.R"                  "Rpdb/distance.dat"           "Rpdb,Distance,"
 echo "Rpdb benchmarks done"
 
 # BioPerl
-run_benchmark $nb "perl BioPerl/parse.pl pdbs/1CRN.pdb"      "BioPerl/parse_1CRN.dat"      "BioPerl,parse 1CRN,"
-run_benchmark $nb "perl BioPerl/parse.pl pdbs/3JYV.pdb"      "BioPerl/parse_3JYV.dat"      "BioPerl,parse 3JYV,"
-run_benchmark $ns "perl BioPerl/parse.pl pdbs/1HTQ.pdb"      "BioPerl/parse_1HTQ.dat"      "BioPerl,parse 1HTQ,"
-run_benchmark $nb "perl BioPerl/count.pl"                    "BioPerl/count.dat"           "BioPerl,count,"
-run_benchmark $nb "perl BioPerl/distance.pl"                 "BioPerl/distance.dat"        "BioPerl,distance,"
+run_benchmark $nb "perl BioPerl/parse.pl pdbs/1CRN.pdb"      "BioPerl/parse_1CRN.dat"      "BioPerl,Parse 1CRN,"
+run_benchmark $nb "perl BioPerl/parse.pl pdbs/3JYV.pdb"      "BioPerl/parse_3JYV.dat"      "BioPerl,Parse 3JYV,"
+run_benchmark $ns "perl BioPerl/parse.pl pdbs/1HTQ.pdb"      "BioPerl/parse_1HTQ.dat"      "BioPerl,Parse 1HTQ,"
+run_benchmark $nb "perl BioPerl/count.pl"                    "BioPerl/count.dat"           "BioPerl,Count,"
+run_benchmark $nb "perl BioPerl/distance.pl"                 "BioPerl/distance.dat"        "BioPerl,Distance,"
 echo "BioPerl benchmarks done"
 
 # BioRuby
-run_benchmark $nb "ruby BioRuby/parse.rb pdbs/1CRN.pdb"      "BioRuby/parse_1CRN.dat"      "BioRuby,parse 1CRN,"
-run_benchmark $nb "ruby BioRuby/parse.rb pdbs/3JYV.pdb"      "BioRuby/parse_3JYV.dat"      "BioRuby,parse 3JYV,"
-run_benchmark $ns "ruby BioRuby/parse.rb pdbs/1HTQ.pdb"      "BioRuby/parse_1HTQ.dat"      "BioRuby,parse 1HTQ,"
-run_benchmark $nb "ruby BioRuby/count.rb"                    "BioRuby/count.dat"           "BioRuby,count,"
-run_benchmark $nb "ruby BioRuby/distance.rb"                 "BioRuby/distance.dat"        "BioRuby,distance,"
+run_benchmark $nb "ruby BioRuby/parse.rb pdbs/1CRN.pdb"      "BioRuby/parse_1CRN.dat"      "BioRuby,Parse 1CRN,"
+run_benchmark $nb "ruby BioRuby/parse.rb pdbs/3JYV.pdb"      "BioRuby/parse_3JYV.dat"      "BioRuby,Parse 3JYV,"
+run_benchmark $ns "ruby BioRuby/parse.rb pdbs/1HTQ.pdb"      "BioRuby/parse_1HTQ.dat"      "BioRuby,Parse 1HTQ,"
+run_benchmark $nb "ruby BioRuby/count.rb"                    "BioRuby/count.dat"           "BioRuby,Count,"
+run_benchmark $nb "ruby BioRuby/distance.rb"                 "BioRuby/distance.dat"        "BioRuby,Distance,"
 echo "BioRuby benchmarks done"
 
 # Victor
-run_benchmark $nb "Victor/parse pdbs/1CRN.pdb"               "Victor/parse_1CRN.dat"       "Victor,parse 1CRN,"
-run_benchmark $nb "Victor/parse pdbs/3JYV.pdb"               "Victor/parse_3JYV.dat"       "Victor,parse 3JYV,"
-run_benchmark $ns "Victor/parse pdbs/1HTQ.pdb"               "Victor/parse_1HTQ.dat"       "Victor,parse 1HTQ,"
+run_benchmark $nb "Victor/parse pdbs/1CRN.pdb"               "Victor/parse_1CRN.dat"       "Victor,Parse 1CRN,"
+run_benchmark $nb "Victor/parse pdbs/3JYV.pdb"               "Victor/parse_3JYV.dat"       "Victor,Parse 3JYV,"
+run_benchmark $ns "Victor/parse pdbs/1HTQ.pdb"               "Victor/parse_1HTQ.dat"       "Victor,Parse 1HTQ,"
 echo "Victor benchmarks done"
 
 # ESBTL
-run_benchmark $nb "ESBTL/parse pdbs/1CRN.pdb"                "ESBTL/parse_1CRN.dat"        "ESBTL,parse 1CRN,"
-run_benchmark $nb "ESBTL/parse pdbs/3JYV.pdb"                "ESBTL/parse_3JYV.dat"        "ESBTL,parse 3JYV,"
+run_benchmark $nb "ESBTL/parse pdbs/1CRN.pdb"                "ESBTL/parse_1CRN.dat"        "ESBTL,Parse 1CRN,"
+run_benchmark $nb "ESBTL/parse pdbs/3JYV.pdb"                "ESBTL/parse_3JYV.dat"        "ESBTL,Parse 3JYV,"
 echo "ESBTL benchmarks done"
 
 # Plot results
